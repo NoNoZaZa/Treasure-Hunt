c8f8cab06b8e81f8d7c91e0731d427470567782c
elenawa <40383608+elenawa@users.noreply.github.com>
2018-07-31 15:39:28 +0200

    Taschenlampe Prototyp
    Taschenlampe mit Spotlight, das man mit der F-Taste ein- & ausschalten kann

---
+++ Erzeugung zufaellige Obj auf Ebene/Assets/Flashlight.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    public Light flashLight;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            flashLight.enabled = !flashLight.enabled;
+        }
+    }
+}
