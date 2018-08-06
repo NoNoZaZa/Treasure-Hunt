23281417cb44bd741da8c049a8ef9cbcbd2050ca
elenawa <40383608+elenawa@users.noreply.github.com>
2018-08-06 09:51:28 +0200

    Beispielszene mit Taschenlampe
    Version 2

---
+++ Erzeugung zufaellige Obj auf Ebene/Assets/Collision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour {
+
+    public int keycount = 0;
+
+    // Event, dass bei der Berührung mit einem anderen ausgeführt wird
+    void OnCollisionEnter(Collision touch)
+    {
+        if (touch.gameObject.name == "key_gold")
+        {
+        keycount++; 
+        }
+    }
+}
