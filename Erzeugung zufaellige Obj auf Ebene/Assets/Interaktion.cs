f3c99930617e79bd017b5df19a29c87fed340b16
Verena <keks.schokolade@web.de>
2018-08-09 14:34:31 +0200

    Kreuz im Bildschirm, Quiz
    erste Ideen für das Skript

---
+++ Erzeugung zufaellige Obj auf Ebene/Assets/Interaktion.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaktion : MonoBehaviour {
+    public Texture2D kreuz;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+    private void OnGUI()
+    {
+        GUI.Box(new Rect(Screen.width / 2, Screen.height / 2, 16, 16), kreuz);
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update () {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(new Vector2(Screen.width /2, Screen.height /2));
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                hit.collider.gameObject.GetComponent<Renderer>().material.color = Color.green;
+            }
+        }
+
+	}
+}
---
+++ Erzeugung zufaellige Obj auf Ebene/Assets/Quizausloeser.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quizausloeser : MonoBehaviour
+{
+
+    public GameObject wand;
+    // Use this for initialization
+    void Start()
+    {
+        //GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        //cube.transform.position = new Vector3(0.0F, 3.5F, 4.5F);
+        //cube.transform.localScale += new Vector3(0.2F, 0.2F, 0.2F);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(UnityEngine.Collision collision)
+    {
+        Debug.Log("Collide!");
+        wand.GetComponent<Renderer>().material.color = Color.red;
+        Destroy(this);
+    }
+
+}
