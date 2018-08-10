eba1ed926ffb70ef9a34449afa6188acbcb22c9a
elenawa <40383608+elenawa@users.noreply.github.com>
2018-08-10 15:07:45 +0200

    Quiz anklickbar
    Quizwürfel auswählbar zur Paarfindung,
    Version noch nicht vollständig

--- Erzeugung zufaellige Obj auf Ebene/Assets/Kreuz/Interaktion.cs
+++ Erzeugung zufaellige Obj auf Ebene/Assets/Kreuz/Interaktion.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Interaktion : MonoBehaviour {
     public Texture2D kreuz;
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+
+    int klicknum = 0;
+    public GameObject[] geklickt;
+
+    void Start () {
+        geklickt = new GameObject[2];
+    }
 
 
     private void OnGUI()
@@ -27,7 +31,34 @@ public class Interaktion : MonoBehaviour {
         {
             if (Input.GetMouseButtonDown(0))
             {
+                klicknum++;
                 hit.collider.gameObject.GetComponent<Renderer>().material.color = Color.green;
+                if (hit.collider.gameObject.tag == "stein")
+                {
+                    GameObject stein = hit.collider.gameObject;
+                    stein.transform.position = new Vector3(stein.transform.position.x, stein.transform.position.y, -0.8f);
+                    if (klicknum == 1 || klicknum == 2)
+                    {
+                        if (klicknum == 1)
+                        {
+                            geklickt[0] = stein;
+                        }
+                        if (klicknum == 2)
+                        {
+                            geklickt[1] = stein;
+                        }
+                        if (geklickt[0].GetComponent<Renderer>().material.color == geklickt[1].GetComponent<Renderer>().material.color)
+                        {
+                            Destroy(geklickt[0]);
+                            Destroy(geklickt[1]);
+                        }
+                    }
+                    else
+                    {
+                        klicknum = 0;
+                    }
+                    
+                }
             }
         }
 
