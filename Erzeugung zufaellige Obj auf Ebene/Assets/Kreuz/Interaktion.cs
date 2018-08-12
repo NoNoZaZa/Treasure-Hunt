436e7a6fca90a39352d6b1fb4c953f32668bf3a3
elenawa <40383608+elenawa@users.noreply.github.com>
2018-08-12 15:04:44 +0200

    Quiz1 Algorithmus fertig
    Paarfindungsalgorithmus fertig gestellt,
    nur Texturen usw. fehlen noch

--- Erzeugung zufaellige Obj auf Ebene/Assets/Kreuz/Interaktion.cs
+++ Erzeugung zufaellige Obj auf Ebene/Assets/Kreuz/Interaktion.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Interaktion : MonoBehaviour {
     public Texture2D kreuz;
@@ -31,31 +32,43 @@ public class Interaktion : MonoBehaviour {
         {
             if (Input.GetMouseButtonDown(0))
             {
-                klicknum++;
-                hit.collider.gameObject.GetComponent<Renderer>().material.color = Color.green;
+                
+                //hit.collider.gameObject.GetComponent<Renderer>().material.color = Color.green;
                 if (hit.collider.gameObject.tag == "stein")
                 {
+                    ++klicknum;
                     GameObject stein = hit.collider.gameObject;
-                    stein.transform.position = new Vector3(stein.transform.position.x, stein.transform.position.y, -0.8f);
+                    
                     if (klicknum == 1 || klicknum == 2)
                     {
                         if (klicknum == 1)
                         {
                             geklickt[0] = stein;
+                            stein.transform.position = new Vector3(stein.transform.position.x, stein.transform.position.y, -0.8f);
                         }
                         if (klicknum == 2)
                         {
                             geklickt[1] = stein;
+                            stein.transform.position = new Vector3(stein.transform.position.x, stein.transform.position.y, -0.8f);
+                            klicknum = 0;
                         }
+                        //Bug "ein Würfel kann in beide Plätze abgespeichert werden, sodass nur dieser verschwindet" beheben
                         if (geklickt[0].GetComponent<Renderer>().material.color == geklickt[1].GetComponent<Renderer>().material.color)
                         {
                             Destroy(geklickt[0]);
                             Destroy(geklickt[1]);
                         }
+                        if (geklickt[0].GetComponent<Renderer>().material.color != geklickt[1].GetComponent<Renderer>().material.color)
+                        {
+                            geklickt[0].transform.position = new Vector3(geklickt[0].transform.position.x, geklickt[0].transform.position.y, 0f);
+                            geklickt[1].transform.position = new Vector3(geklickt[1].transform.position.x, geklickt[1].transform.position.y, 0f);
+                            Array.Clear(geklickt, 0, 2);
+                        }
                     }
                     else
                     {
-                        klicknum = 0;
+                        
+                        
                     }
                     
                 }
