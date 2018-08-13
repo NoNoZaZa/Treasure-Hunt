4da21307bd1cc9c5db07073500ce7a01be9b194a
elenawa <40383608+elenawa@users.noreply.github.com>
2018-08-13 17:04:30 +0200

    Quiz4 version 1.2
    Drehplatten optisch erzeugt

--- Erzeugung zufaellige Obj auf Ebene/Assets/Quiz/Quiz4/Quiz4.cs
+++ Erzeugung zufaellige Obj auf Ebene/Assets/Quiz/Quiz4/Quiz4.cs
@@ -5,22 +5,49 @@ using UnityEngine;
 public class Quiz4 : MonoBehaviour {
     public GameObject drehrad1, drehrad2, drehrad3;
     public Vector3 groesse = new Vector3(0.8f, 0.8f, 0.8f);
-    public Vector3 cubePos = new Vector3(2f, 0f, 3f);
+    Vector3 ecke11, ecke23, ecke34;
 
     // Use this for initialization
     void Start()
     {
-        GameObject rad1 = Instantiate(drehrad1, cubePos, Quaternion.identity);
+        GameObject rad1 = Instantiate(drehrad1, drehrad1.transform.position, Quaternion.identity);
         if (!rad1.activeInHierarchy) rad1.SetActive(true);
-        GameObject rad2 = Instantiate(drehrad2, cubePos, Quaternion.identity);
+        GameObject rad2 = Instantiate(drehrad2, drehrad2.transform.position, Quaternion.identity);
         if (!rad2.activeInHierarchy) rad2.SetActive(true);
-        GameObject rad3 = Instantiate(drehrad3, cubePos, Quaternion.identity);
+        GameObject rad3 = Instantiate(drehrad3, drehrad3.transform.position, Quaternion.identity);
         if (!rad3.activeInHierarchy) rad3.SetActive(true);
-
+        symboleZuewisen();
+        vergleichen();
     }
 
-    // Update is called once per frame
-    void Update () {
+    void symboleZuewisen()
+    {
+        Mesh mesh1 = drehrad1.GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices1 = mesh1.vertices;
+        ecke11 = vertices1[0];
+
+        Mesh mesh2 = drehrad1.GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices2 = mesh2.vertices;
+        ecke11 = vertices2[2];
+
+        Mesh mesh3 = drehrad1.GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices3 = mesh3.vertices;
+        ecke34 = vertices3[3];
+
+    }
+    void vergleichen()
+    {
+        if (ecke11 == ecke23 && ecke11 == ecke34)
+        {
+            Destroy(drehrad1);
+            Destroy(drehrad2);
+            Destroy(drehrad3);
+        }
+    }
+
+
+        // Update is called once per frame
+        void Update () {
 		
 	}
 }
