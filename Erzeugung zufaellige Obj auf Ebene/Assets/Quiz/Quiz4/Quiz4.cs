c2701e20790741f5204b49c47c015e911bcbce3a
elenawa <40383608+elenawa@users.noreply.github.com>
2018-08-13 16:55:09 +0200

    Quiz4 angefangen
    Drehräder erstellt

---
+++ Erzeugung zufaellige Obj auf Ebene/Assets/Quiz/Quiz4/Quiz4.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quiz4 : MonoBehaviour {
+    public GameObject drehrad1, drehrad2, drehrad3;
+    public Vector3 groesse = new Vector3(0.8f, 0.8f, 0.8f);
+    public Vector3 cubePos = new Vector3(2f, 0f, 3f);
+
+    // Use this for initialization
+    void Start()
+    {
+        GameObject rad1 = Instantiate(drehrad1, cubePos, Quaternion.identity);
+        if (!rad1.activeInHierarchy) rad1.SetActive(true);
+        GameObject rad2 = Instantiate(drehrad2, cubePos, Quaternion.identity);
+        if (!rad2.activeInHierarchy) rad2.SetActive(true);
+        GameObject rad3 = Instantiate(drehrad3, cubePos, Quaternion.identity);
+        if (!rad3.activeInHierarchy) rad3.SetActive(true);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
