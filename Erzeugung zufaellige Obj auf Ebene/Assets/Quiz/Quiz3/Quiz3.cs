9cb59dde067fe45e81dedb13ae15da93623e2c07
Verena <keks.schokolade@web.de>
2018-08-13 12:31:22 +0200

    Quiz3 mit richtigem Auslöser
    Auslöser verbessert

--- Erzeugung zufaellige Obj auf Ebene/Assets/Quiz/Quiz3/Quiz3.cs
+++ Erzeugung zufaellige Obj auf Ebene/Assets/Quiz/Quiz3/Quiz3.cs
@@ -8,6 +8,7 @@ public class Quiz3 : MonoBehaviour {
     public float maxGeschw = 6f;
     private float geschwindigkeit;
     public float grenze = 3f;
+    public float offset; 
    // Use this for initialization
 
     void Start()
@@ -18,7 +19,7 @@ public class Quiz3 : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(Mathf.PingPong(Time.time * geschwindigkeit, grenze), transform.position.y, transform.position.z);
+        transform.position = new Vector3(Mathf.PingPong(Time.time * geschwindigkeit, grenze) + offset, transform.position.y, transform.position.z);
         //transform.Translate(Vector3.left* geschwindigkeit * Time.deltaTime);
     }
 
---
+++ Erzeugung zufaellige Obj auf Ebene/Assets/Quiz/Quiz3/Quiz3Ausloeser.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quiz3Ausloeser : MonoBehaviour
+{
+
+    public GameObject quiz3Cube;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(UnityEngine.Collision collision)
+    {
+        Debug.Log("Collide!");
+        quiz3Cube.SetActive(true);
+        Destroy(this.gameObject);
+    }
+}
