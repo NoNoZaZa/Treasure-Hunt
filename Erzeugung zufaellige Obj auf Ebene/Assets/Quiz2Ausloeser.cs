85faa0cf29b50390718b91968073000caab33f7d
elenawa <40383608+elenawa@users.noreply.github.com>
2018-08-14 14:04:43 +0200

    Quiz2 mit Ausloeser
    immer noch mit Kollisionsproblemen

---
+++ Erzeugung zufaellige Obj auf Ebene/Assets/Quiz2Ausloeser.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quiz2Ausloeser : MonoBehaviour
+{
+
+    public GameObject quiz2Cube;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(UnityEngine.Collision collision)
+    {
+        Destroy(this.gameObject);
+        quiz2Cube.SetActive(true);
+        Destroy(this.gameObject);
+
+    }
+}
