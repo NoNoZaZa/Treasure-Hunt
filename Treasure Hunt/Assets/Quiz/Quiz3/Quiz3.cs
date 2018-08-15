9bafc62b2d1f4ce1e56b0d5e504db8128751bc9b
Verena <keks.schokolade@web.de>
2018-08-15 16:29:01 +0200

    Timer läuft
    Läuft ab Spielbeginn, muss noch geändert werden!

--- Treasure Hunt/Assets/Quiz/Quiz3/Quiz3.cs
+++ Treasure Hunt/Assets/Quiz/Quiz3/Quiz3.cs
@@ -9,21 +9,61 @@ public class Quiz3 : MonoBehaviour {
     private float geschwindigkeit;
     public float grenze = 3f;
     public float offset;
+    //private float groesse = 0.8f;
     public List<GameObject> cubeListe;
-   // Use this for initialization
+    //public GameObject cubeForm;
+    // Use this for initialization
 
     void Start()
     {
+        //ErzeugungCubes();
+        //AnordnungListe();
         geschwindigkeit = Random.Range(minGeschw, maxGeschw);
+        
+        
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        //CubesBewegen();
         transform.position = new Vector3(Mathf.PingPong(Time.time * geschwindigkeit, grenze) + offset, transform.position.y, transform.position.z);
-        //transform.Translate(Vector3.left* geschwindigkeit * Time.deltaTime);
     }
 
-   
-    
+    //void ErzeugungCubes()
+    //{
+    //    for (int i = 0; i < 6; i++)
+    //    {
+    //        GameObject cube = Instantiate(cubeForm, transform.position, transform.rotation);
+    //        cube.transform.localScale = new Vector3(groesse, groesse, groesse);
+    //        cubeListe.Add(cube);
+    //    }
+    //}
+
+    //void AnordnungListe()
+    //{
+    //    for(int j =0; j< cubeListe.Count; j++)
+    //    {
+    //        cubeListe[j].transform.Translate(0, j, 0);
+    //    }
+    //}
+
+    //void CubesBewegen()
+    // {  
+    //foreach(GameObject element in cubeListe) { 
+    //  transform.position = new Vector3(
+    //  Mathf.PingPong(Time.time * geschwindigkeit, grenze) + offset, transform.position.y, transform.position.z);
+    //      Debug.Log("Funktioniert");
+
+    //  }
+    //         //transform.Translate(Vector3.left* geschwindigkeit * Time.deltaTime);
+    //     for(int c=0; c < cubeListe.Count; c++)
+    //     {
+    //         cubeListe[c].transform.position = new Vector3(
+    //         Mathf.PingPong(Time.time * geschwindigkeit, grenze) + offset, transform.position.y, transform.position.z);
+    //         Debug.Log(c);
+    //     }
+    // }
+
 }
