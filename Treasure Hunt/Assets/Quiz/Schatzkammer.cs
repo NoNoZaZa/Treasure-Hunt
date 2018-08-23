01652ac3adb0749e43a00c51d22fb23b2bd8ed09
Verena <keks.schokolade@web.de>
2018-08-23 14:48:40 +0200

    Schatzkammertext und Texturen geändert

---
+++ Treasure Hunt/Assets/Quiz/Schatzkammer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Schatzkammer : MonoBehaviour
+{
+    GameManager gameManager;
+    int keysCollected;
+    public Text scoreText;
+    public GameObject scoreAnzeige;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        keysCollected = gameManager.keysCollected;
+        if (keysCollected == 1)
+        {
+            scoreAnzeige.SetActive(true);
+            scoreText.text = "SPIELENDE \n\nErreichte Punkte: 500 \n\nDrücke ESC zum Verlassen.";
+        }
+
+
+    }
+    private void OnCollisionEnter(UnityEngine.Collision collision)
+    {
+        
+
+
+    }
+}
